e0015ad45bcde6bad9f37aee03717a5c86f57070
Per Valentin <per.valentin@quenchtec.com>
2021-02-15 15:56:16 +0100

    Update dc.vis.js

--- dc.vis.js
+++ dc.vis.js
@@ -1,9 +1,9 @@
 /*
 * Test function for buttons.
 * Writes a message containing parameter information to console.
-* qno: question ID.
-* subqIndex: zero based subq index.
-* paramObj: object with settings as properties.
+* qno: Question ID.
+* subqIndex: Zero based subq index.
+* paramObj: Object with settings as properties.
 */
 function rsVisButton(qno, subqIndex, paramObj) {
     var msg = "This is the rsVisButton function\n qno: " + qno + ", subqIndex: " + subqIndex + "\nObject properties:\n";
