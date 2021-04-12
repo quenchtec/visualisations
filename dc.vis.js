3de5eb87baa10ce428494c8006206dcbfa3074f4
Per Valentin <per.valentin@quenchtec.com>
2021-04-12 09:49:50 +0200

    rsVisScrollingGrid added

--- dc.vis.js
+++ dc.vis.js
@@ -14,4 +14,20 @@ function rsVisButton(qno, subqIndex, paramObj) {
     console.log(msg);
 }
 
+/*
+* Test function for scrolling grids.
+* Writes a message containing parameter information to console.
+* qno: Question ID.
+* subqIndex: Zero based subq index.
+* paramObj: Object with settings as properties.
+*/
+function rsVisScrollingGrid(qno, subqIndex, paramObj) {
+    var msg = "This is the rsVisButton function\n qno: " + qno + ", subqIndex: " + subqIndex + "\nObject properties:\n";
+    for (const p in paramObj) {
+        msg += ` ${p}: ${paramObj[p]}\n`;
+    }
+
+    console.log(msg);
+}
+
 $.getScript("https://static.quenchtec.net/46033bcf-0124-4b26-b4ea-30726bd5357c/9rdiDsHsCw7/newBtniQuest.js");
