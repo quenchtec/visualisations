03a481d0cead4c7d4db2245a12e373d7976b0c06
Per Valentin <per.valentin@quenchtec.com>
2023-01-11 16:34:38 +0100

    Update dc.vis.js

--- dc.vis.js
+++ dc.vis.js
@@ -3,7 +3,7 @@
 * Writes a message containing parameter information to console.
 * qno: Question ID.
 * subqIndex: Zero based subq index.
-* paramObj: Object with settings as properties.
+* paramObj: Object with settings as properties
 */
 function rsVisButton(qno, subqIndex, paramObj) {
     //var msg = "This is the rsVisButton function\n qno: " + qno + ", subqIndex: " + subqIndex + "\nObject properties:\n";
