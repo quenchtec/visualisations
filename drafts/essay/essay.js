fed2c36cb7dcffc5bbd9dc63bfd36121317140a4
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2023-02-13 16:42:19 +0000

    Updating JS to allow multiple essays in future
    Changed from a static update to going via all text area elements.
    Changed the key up event to .on() with key up and down event.
    Question will now work on checkboxes, but should not be used on any for now.

--- drafts/essay/essay.js
+++ drafts/essay/essay.js
@@ -1,28 +1,31 @@
-
-var rsEssay = (function () {
-	rsParams.blnShowProgress = (typeof rsParams.blnShowProgress === "undefined") ? true : rsParams.blnShowProgress;
-
-
-  if(rsParams.blnShowProgress){
-
-	  rsParams.blnProgressBarText = (typeof rsParams.blnProgressBarText === "undefined") ? true : rsParams.blnProgressBarText;
-	  rsParams.intIdealLength = (typeof rsParams.intIdealLength === "undefined") ? 200 : rsParams.intIdealLength;
-	  rsParams.strProgressText = (typeof rsParams.strProgressText === "undefined") ? "" : rsParams.strProgressText;
-
+var rsEssay = (function() {
+  rsParams.blnShowProgress = (typeof rsParams.blnShowProgress === "undefined") ? true : rsParams.blnShowProgress;
+  if (rsParams.blnShowProgress) {
+    rsParams.blnProgressBarText = (typeof rsParams.blnProgressBarText === "undefined") ? true : rsParams.blnProgressBarText;
+    rsParams.intIdealLength = (typeof rsParams.intIdealLength === "undefined") ? 200 : rsParams.intIdealLength;
+    rsParams.strProgressText = (typeof rsParams.strProgressText === "undefined") ? "" : rsParams.strProgressText;
     let intBoxWidth = $('.cTextArea')[0].clientWidth;
-    let arrMessage = rsParams.strProgressText.split("|");
-    $(".cTextArea ").after("<div class='essay-progress-bar' style='width: " + intBoxWidth + "px;'><span class='progress-bar-messages'></span><div class='essay-progress-fill'></div></div>");
-    $(".cTextArea ").keyup(function() {OETool(rsParams.intIdealLength, arrMessage);});
+    let arrMessage = rsParams.strProgressText.split("#|#");//this is the separator for multiple essays with progress bars
+    $(arrMessage).each(function(e){
+        arrMessage[e] = arrMessage[e].split("|");
+    });
+
+    $(".cTextArea").each(function(e){
+      $(this).after("<div class='essay-progress-bar' style='width: " + intBoxWidth + "px;'><span class='progress-bar-messages'></span><div class='essay-progress-fill'></div></div>");
+      $(this).on("keydown keyup", function() {OETool(rsParams.intIdealLength, arrMessage[e], e);});
+    });
   }
-function OETool(intIdealLength, arrMessage) {
-  let arrMessages = arrMessage;
-  let x = 0;
-  let intIL = intIdealLength;
-  let stringInputed = $(".cTextArea").val().length;
-  var intStep = 100 / arrMessages.length;
-  if ((intIL > 0) && (stringInputed > 0)) {x = stringInputed / (intIL / 100);}
-  if (x < 100) {$(".essay-progress-fill").width(x + "%");}
-  else {$(".essay-progress-fill").width("100%");}
-  if(rsParams.blnProgressBarText){$(".progress-bar-messages").text(arrMessages[Math.floor(x / intStep)]);}
-}
+
+  function OETool(intIdealLength, arrMessage, intInd) {
+    let intIndex = intInd;
+    let arrMessages = arrMessage;
+    let x = 0;
+    let intIL = intIdealLength;
+    let stringInputed = $(".cTextArea").eq(intIndex).val().length;
+    var intStep = 100 / arrMessages.length;
+    if ((intIL > 0) && (stringInputed > 0)) {x = stringInputed / (intIL / 100);}
+    if (x < 100) {$(".essay-progress-fill").eq(intIndex).width(x + "%");}
+    else {$(".essay-progress-fill").eq(intIndex).width("100%");}
+    if (rsParams.blnProgressBarText) {$(".progress-bar-messages").eq(intIndex).text(arrMessages[Math.floor(x / intStep)]);}
+  }
 })();
