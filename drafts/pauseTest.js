8494303e7c4e6a950c34002df00db4cc35b9acb8
Bjørn Grønli <bjorn.gronli@quenchtec.com>
2023-02-15 15:28:57 +0100

    Create pauseTest.js

---
+++ drafts/pauseTest.js
@@ -0,0 +1,9 @@
+function syncDelay(milliseconds){
+    var start = new Date().getTime();
+    var end = 0;
+
+    while((end - start) < milliseconds){
+        end = new Date().getTime();
+    }
+  alert("wait done");
+}
