520e8f8d6196e590a5f06debbe8d8f5aa6730822
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2023-06-15 13:24:28 +0100

    Update cqmain.js
    Add a class + some updates

--- drafts/public/oes/essay/v1.0.0/cqmain.js
+++ drafts/public/oes/essay/v1.0.0/cqmain.js
@@ -1,16 +1,26 @@
 function rsEssay(rsQno,rsSubqIndex,rsParams) {
+
+    //Check for WCAG, if the flag is set, we do not do anything as these buttons are not WCAG compliant at crrent
+  if ($('#btnToggleWcag').val() == 1) {
+    return false;
+  }
+  let QuestionID = "#" + rsQno; //This we use in selectors to stay within the question
+  let SubQIndex = rsSubqIndex; //This we use in selectors to stay within the question
+  $(QuestionID).find(".rsRowOpen").addClass("rsCuQeEssay");
+  
+  
   rsParams.blnShowProgress = (typeof rsParams.blnShowProgress === "undefined") ? true : rsParams.blnShowProgress;
   if (rsParams.blnShowProgress) {
     rsParams.blnProgressBarText = (typeof rsParams.blnProgressBarText === "undefined") ? true : rsParams.blnProgressBarText;
     rsParams.intIdealLength = (typeof rsParams.intIdealLength === "undefined") ? 200 : rsParams.intIdealLength;
     rsParams.strProgressText = (typeof rsParams.strProgressText === "undefined") ? "" : rsParams.strProgressText;
-    let intBoxWidth = $('.cTextArea')[0].clientWidth;
+    let intBoxWidth = $(QuestionID).find('.cTextArea')[0].clientWidth;
     let arrMessage = rsParams.strProgressText.split("#|#");//this is the separator for multiple essays with progress bars
     $(arrMessage).each(function(e){
         arrMessage[e] = arrMessage[e].split("|");
     });
 
-    $(".cTextArea").each(function(e){
+    $(QuestionID).find(".cTextArea").each(function(e){
       $(this).after("<div class='essay-progress-bar' style='width: " + intBoxWidth + "px;'><span class='progress-bar-messages'></span><div class='essay-progress-fill'></div></div>");
       $(this).on("keydown keyup", function() {OETool(rsParams.intIdealLength, arrMessage[e], e);});
     });
@@ -21,7 +31,7 @@ function rsEssay(rsQno,rsSubqIndex,rsParams) {
     let arrMessages = arrMessage;
     let x = 0;
     let intIL = intIdealLength;
-    let stringInputed = $(".cTextArea").eq(intIndex).val().length;
+    let stringInputed = $(QuestionID).find(".cTextArea").eq(intIndex).val().length;
     var intStep = 100 / arrMessages.length;
     if ((intIL > 0) && (stringInputed > 0)) {x = stringInputed / (intIL / 100);}
     if (x < 100) {$(".essay-progress-fill").eq(intIndex).width(x + "%");}
