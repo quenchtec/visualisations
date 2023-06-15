9e4eee69bc018a32098a852c4e526a95a7a30189
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2023-06-15 15:44:03 +0100

    Uploading regular text area folder with files

---
+++ drafts/public/oes/textarea/v1.0.0/cqmain.js
@@ -0,0 +1,11 @@
+function rsTxtArea(rsQno,rsSubqIndex,rsParams) {
+
+    //Check for WCAG, if the flag is set, we do not do anything as these buttons are not WCAG compliant at crrent
+  if ($('#btnToggleWcag').val() == 1) {
+    return false;
+  }
+  let QuestionID = "#" + rsQno; //This we use in selectors to stay within the question
+  let SubQIndex = rsSubqIndex; //This we use in selectors to stay within the question
+  $(QuestionID).find(".rsRowOpen").addClass("rsCuQeTextArea");
+ 
+};
---
+++ drafts/public/oes/textarea/v1.0.0/readme.md
@@ -0,0 +1,11 @@
+#############################
+
+------------- TEXT AREA ---------
+#############################
+
+TEXT AREA (open end) Radio/Checkbox based question with no inputs features v1.0.0 Created by Mihail Borisov
+
+For the free use of all WALR clients/customers free of charge.
+
+Features:
+  Nothing special
