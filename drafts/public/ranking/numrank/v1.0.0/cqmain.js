b643018ba0a2f91f7615f096895abfb3102daf69
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2023-07-21 12:01:57 +0100

    Update cqmain.js
    fixed ranking

--- drafts/public/ranking/numrank/v1.0.0/cqmain.js
+++ drafts/public/ranking/numrank/v1.0.0/cqmain.js
@@ -1,68 +1,69 @@
 function ClickRank(rsQno, rsSubqIndex, rsParams) {
-  //Check for WCAG, if the flag is set, we do not do anything as these buttons are not WCAG compliant at crrent
-  if ($('#btnToggleWcag').val() == 1) {
-    return false;
-  }
-  //These parameters come from the settings the user have selected
-  rsParams.autonext = (typeof rsParams.autonext === "undefined") ? true : rsParams.autonext;
-  rsParams.min = (typeof rsParams.min === "undefined") ? 0 : rsParams.min;
-  rsParams.max = (typeof rsParams.max === "undefined") ? 0 : rsParams.max;
-  rsParams.hideNextMin = (typeof rsParams.hideNextMin === "undefined") ? true : rsParams.hideNextMin;
-  rsParams.prescript = (typeof rsParams.prescript === "undefined") ? "" : rsParams.prescript;
-  rsParams.postscript = (typeof rsParams.postscript === "undefined") ? "" : rsParams.postscript;
-
-  //Check for prescript
-  if (rsParams.prescript.length > 0) sParam.prescript;
-
-  //Since there can be mutiple questions on a page, we must make sure all selectors are addressing the proper question (and subquestion) only
-  const QuestionID = "#" + rsQno; //This we use in selectors to stay within the question
-  const btnDivID = "#" + rsQno + "_btn"; //We create our own ID's that will be unique within the page and question
-  const btnDivIDattr = rsQno + "_btn"; //Same id without # to use when we set the ID
-
-  //Turn of autonext if more than one question/subquestion on the page
-  if ($('.cCellSubQuestion').length > 1) {
-    rsParams.autonext = false;
-  }
-
-  let index = 0;
-  $(QuestionID).find('.rsRow').each(function() {
-    $(this).css('justify-content', 'start');
-    $(this).click(function() {
-      checkClick(rsQno, rsParams, this);
-    });
-    $(this).find('td').each(function(i) {
-      if (i == 0) {
-        $(this).css('order', 2);
-      }
-      if (i == 1) {
-        $(this).css({
-          'order': '1',
-        });
-        let value = $(this).find('.cFInput').val();
-        $(this).find('.cFInput').addClass('rsBtnRank');
-        $(this).find('.cFInput').attr('readonly', true);
-        if (value != '') {
-          index++;
-          $(this).find('.cFInput').addClass('rsBtnRankSelected');
-        }
-      }
-      $(QuestionID).find('.cCellSubQuestion').attr('data-index', index);
-    });
-    //Check for hide min
-    if (rsParams.min > 0 && $('.cCellSubQuestion').length == 1) {
-      if (index < rsParams.min) $('#btnNext').hide();
+    //Check for WCAG, if the flag is set, we do not do anything as these buttons are not WCAG compliant at crrent
+    if ($('#btnToggleWcag').val() == 1) {
+        return false;
     }
-  });
-  //Check for postscript
-  if (rsParams.postscript.length > 0) sParam.postscript;
+    //These parameters come from the settings the user have selected
+    rsParams.autonext = (typeof rsParams.autonext === "undefined") ? true : rsParams.autonext;
+    rsParams.min = (typeof rsParams.min === "undefined") ? 0 : rsParams.min;
+    rsParams.max = (typeof rsParams.max === "undefined") ? 0 : rsParams.max;
+    rsParams.hideNextMin = (typeof rsParams.hideNextMin === "undefined") ? true : rsParams.hideNextMin;
+    rsParams.prescript = (typeof rsParams.prescript === "undefined") ? "" : rsParams.prescript;
+    rsParams.postscript = (typeof rsParams.postscript === "undefined") ? "" : rsParams.postscript;
+    var index = 0;
+
+    //Check for prescript
+    if (rsParams.prescript.length > 0) rsParams.prescript;
+
+    //Since there can be mutiple questions on a page, we must make sure all selectors are addressing the proper question (and subquestion) only
+    const QuestionID = "#" + rsQno; //This we use in selectors to stay within the question
+    const btnDivID = "#" + rsQno + "_btn"; //We create our own ID's that will be unique within the page and question
+    const btnDivIDattr = rsQno + "_btn"; //Same id without # to use when we set the ID
+
+    //Turn of autonext if more than one question/subquestion on the page
+    if ($('.cCellSubQuestion').length > 1) {
+        rsParams.autonext = false;
+    }
+
+    $(QuestionID).find('.rsRow').each(function() {
+        $(this).css('justify-content', 'start');
+        $(this).click(function() {
+            checkClick(rsQno, rsParams, this);
+        });
+        $(this).find('td').each(function(i) {
+            if (i == 0) {
+                $(this).css('order', 2);
+            }
+            if (i == 1) {
+                $(this).css({
+                    'order': '1',
+                });
+                let value = $(this).find('.cFInput').val();
+                $(this).find('.cFInput').addClass('rsBtnRank');
+                $(this).find('.cFInput').attr('readonly', true);
+                if (value != '') {
+                    index++;
+                    $(this).find('.cFInput').addClass('rsBtnRankSelected');
+                }
+            }
+            $(QuestionID).find('.cCellSubQuestion').attr('data-index', index);
+        });
+        //Check for hide min
+        if (rsParams.min > 0 && $('.cCellSubQuestion').length == 1) {
+            if (index < rsParams.min) $('#btnNext').hide();
+        }
+    });
+    //Check for postscript
+    if (rsParams.postscript.length > 0) rsParams.postscript;
 }
 
 function checkClick(rsQno, rsParams, item) {
-  const QuestionID = "#" + rsQno; //This we use in selectors to stay within the question	
+  const QuestionID = "#" + rsQno;
   let index = $(QuestionID).find('.cCellSubQuestion').attr('data-index');
   const value = $(item).find('.cFInput').val();
-  if (value == '') {
-    //We clicked an empty row
+  
+  if (value === '') {
+    // We clicked an empty row
     if (rsParams.max > 0 && index >= rsParams.max) {
       return;
     }
@@ -78,20 +79,27 @@ function checkClick(rsQno, rsParams, item) {
       if (index == rsParams.max) $('#btnNext').click();
     }
   } else {
-    //We clicked on an already ranked row
+    // We clicked on an already ranked row
     index = index - 1;
     if (index < 0) index = 0;
     $(item).find('.cFInput').removeClass('rsBtnRankSelected');
     $(item).find('.cFInput').val('');
     $(QuestionID).find('.cCellSubQuestion').attr('data-index', index);
+
+    // Recalculate the rankings for remaining clicked boxes
     $(QuestionID).find('.rsRow').each(function() {
-      const thisValue = $(this).find('.cFInput').val();
-      if (thisValue != '') {
-        if (thisValue > value) {
+      const thisValue = parseInt($(this).find('.cFInput').val());
+      if (thisValue !== '' && thisValue > value) {
+        // If thisValue is double-digit or greater than 10, decrement it by 1
+        if (thisValue >= 10 || thisValue >= rsParams.min) {
           $(this).find('.cFInput').val(thisValue - 1);
+        } else {
+          // If thisValue is single-digit and less than 10, clear it
+          $(this).find('.cFInput').val('');
         }
       }
     });
+
     if (rsParams.min > 0 && $('.cCellSubQuestion').length == 1) {
       if (index < rsParams.min) $('#btnNext').hide();
     }
