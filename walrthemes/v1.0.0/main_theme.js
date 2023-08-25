66f2ef1a6ba68991cf40ba3d1f7d09553c3ec3ef
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2023-08-25 13:34:54 +0100

    Update main_theme.js

--- walrthemes/v1.0.0/main_theme.js
+++ walrthemes/v1.0.0/main_theme.js
@@ -1,3 +1,5 @@
+/*
+
 $(document).ready(function() {
     var targetNode = $('.rsPanelMain')[0];
     var config = { attributes: true, childList: true, subtree: true };
@@ -56,3 +58,57 @@ function cthemePageReady() {
     }
 
 }
+*/
+$(document).ready(function() {
+    var targetNode = $('.rsPanelMain')[0];
+    var config = { attributes: true, childList: true, subtree: true };
+    var callback = function(mutationsList, observer) {
+        for(var mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                cthemePageReady(); // Call your function here
+            }
+        }
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+});
+
+function cthemePageReady() {
+    ghostText("Please, type in...");
+    gridUpdate();
+    
+    
+    function isMobileDevice() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    }
+    
+    function ghostText(custText) {
+        console.log("TPR GT call");
+        $('.cTextInput').each(function () {
+            $(this).attr("placeholder", custText);
+        });
+    }
+    
+    function gridUpdate() {
+        if (($(".cTable").hasClass("rsSingleGrid") || $(".cTable").hasClass("rsMultiGrid")) && (!$(".cTable").hasClass("rsCQ"))) {
+            let gridID = $(".rsSingleGrid, .rsMultiGrid").prop("id");
+            let gridIND = gridID.split("_")[1];
+            //rearrange the grid for mobiles
+            if (isMobileDevice()) {
+                $(".rsRow").each(function () { $(this).children(".cCell").each(function (e) { $(this).append($("#h_" + gridIND + "_" + e).clone()); }); });
+                $("td.cCellHeader").parent().remove();
+            } else {
+                let colLength = $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").length;
+                var cHeight = 19;
+                $(".cCellFirstHeader").css("min-width", 100 / colLength + 'rem');
+                $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (100 / colLength) / 2 + 'rem');
+                setTimeout(function () {
+                    $('.rsRow').find(".rs-ht").each(function () { if ($(this)[0].clientHeight > cHeight) { cHeight = $(this)[0].clientHeight; } });
+                    $(".rsRow").each(function () { $(this).css("height", cHeight + "px"); });
+                }, 100);
+            }
+        }
+    }
+}
+
+
