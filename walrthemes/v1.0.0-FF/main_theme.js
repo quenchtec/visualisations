b1e8cae2f4279299e492a8260d31cda7f956bde2
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2023-10-03 18:26:31 +0100

    Update main_theme.js

--- walrthemes/v1.0.0-FF/main_theme.js
+++ walrthemes/v1.0.0-FF/main_theme.js
@@ -1,71 +1,3 @@
-
-/*
-$(document).ready(function() {
-                console.log("doc ready");
-    var targetNode = document.getElementById('rsPanelMain');
-    var config = { attributes: true, childList: true, subtree: true };
-    var callback = function(mutationsList, observer) {
-        for(var mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                console.log("call c theme");
-                cthemePageReady(); // Call your function here
-              break;
-            }
-        }
-    };
-    var observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-});
-
-
-
-//themePageReady();
-function cthemePageReady() {
-    //alert("test");
-    //console.log("TPR call");
-    //$(document).ready(function () {
-        ghostText("Please, type in...");
-        gridUpdate();
-            console.log("afterc all");
-
-    //});
-
-    function isMobileDevice() {
-        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    }
-    function ghostText(custText) {
-        //console.log("TPR GT call");
-        $('.cTextInput').each(function () {
-            $(this).attr("placeholder", custText);
-        });
-    }
-    function gridUpdate() {
-        console.log("TPR GU call");
-
-        if (($(".cTable").hasClass("rsSingleGrid") || $(".cTable").hasClass("rsMultiGrid")) && (!$(".cTable").hasClass("rsCQ"))) {
-            let gridID = $(".rsSingleGrid, .rsMultiGrid").prop("id");
-            let gridIND = gridID.split("_")[1];
-            //rearrange the grid for mobiles
-            if (isMobileDevice()) {
-                $(".rsRow").each(function () { $(this).children(".cCell").each(function (e) { $(this).append($("#h_" + gridIND + "_" + e).clone()); }); });
-                $("td.cCellHeader").parent().remove();
-            } else {
-                let colLength = $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").length;
-                var cHeight = 19;
-                $(".cCellFirstHeader").css("min-width", 100 / colLength + 'rem');
-                $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (100 / colLength) / 2 + 'rem');
-                setTimeout(function () {
-                    $('.rsRow').find(".rs-ht").each(function () { if ($(this)[0].clientHeight > cHeight) { cHeight = $(this)[0].clientHeight; } });
-                    $(".rsRow").each(function () { $(this).css("height", cHeight + "px"); });
-                }, 100);
-            }
-        }
-    }
-}
-
-
-*/
-
 $(document).ready(function() {
     // Create a MutationObserver when the document is fully loaded
    //console.log("doc ready");
@@ -78,10 +10,11 @@ $(document).ready(function() {
                 //console.log("call the theme in mutation");
 
                 cthemePageReady(); // Call your function here
-                break; // We've handled the mutation, no need to continue
                 $(".cTable").click(function() {
                   putSomeClasses($(this))
                 });
+
+                break; // We've handled the mutation, no need to continue
             }
         }
     });
