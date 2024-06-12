8a9cb221400778d181dc7f14b8eb794a63405793
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2024-06-12 13:54:04 +0100

    Update main_theme.js

--- walrthemes/v1.0.0-2colour/main_theme.js
+++ walrthemes/v1.0.0-2colour/main_theme.js
@@ -212,7 +212,7 @@ function gridUpdate(grid_this) {
 
               $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("min-width", (50 / colLength-1) + '%');
               //$('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (gridWidth / (colLength+1)) + 'px');
-              $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (50 / colLength-1) + '%');
+              $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (80 / colLength-1) + '%');
 
               $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").each(function(){if(cWidth < $(this)[0].clientWidth) {cWidth = $(this)[0].clientWidth;$(this).addClass("revisedColumn");}});
               //$('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("min-width", (cWidth * 0.8) + 'px');
