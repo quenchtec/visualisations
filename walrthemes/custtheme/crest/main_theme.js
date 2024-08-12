af6d53a6b79247af554569e832df5ef9060364dc
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2024-08-12 16:05:37 +0100

    Update main_theme.js

--- walrthemes/custtheme/crest/main_theme.js
+++ walrthemes/custtheme/crest/main_theme.js
@@ -248,9 +248,9 @@ function gridUpdate(grid_this) {
               $('#' + gridID).find(".cCellFirstHeader").css("width", ((gridWidth * 1.2) / colLength) + 'px');
               $('#' + gridID).find(".cCellFirstHeader").addClass("revisedCellFirstHeader");
 
-              $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("min-width", (50 / colLength-1) + '%');
+              $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("min-width", (50 / (colLength-1)) + '%');
               //$('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (gridWidth / (colLength+1)) + 'px');
-              $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (50 / colLength-1) + '%');
+              $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("width", (50 / (colLength-1)) + '%');
 
               $('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").each(function(){if(cWidth < $(this)[0].clientWidth) {cWidth = $(this)[0].clientWidth;$(this).addClass("revisedColumn");}});
               //$('#' + gridID).find(".cCellHeader").not(".cCellFirstHeader").css("min-width", (cWidth * 0.8) + 'px');
