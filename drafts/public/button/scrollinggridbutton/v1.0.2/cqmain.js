8a05ecc243800cf7b75f9d2cf3c9a29d215e451c
MBorWalr <122782181+MBorWalr@users.noreply.github.com>
2024-10-22 13:44:29 +0100

    Update cqmain.js

--- drafts/public/button/scrollinggridbutton/v1.0.2/cqmain.js
+++ drafts/public/button/scrollinggridbutton/v1.0.2/cqmain.js
@@ -288,9 +288,13 @@ function rsVisScrollingGrid(rsQno, rsSubqIndex, rsParams) {
     if (intNumButtonsPrRow > 0) {
       pctWidth = parseInt(100 / intNumButtonsPrRow);
     }
-  
+
+    let typeOfinp = (intNumChecks) ? "checkbox" : "radio"; 
+
     //Build HTML for buttons etc
-    strHTML = '<div id="' + btnDivIDattr + '" class="rsFlexBtnContainer" width="100%">';
+    //strHTML = '<div id="' + btnDivIDattr + '" class="rsFlexBtnContainer rsBtnGrid" width="100%">';
+    strHTML = `<div id="${btnDivIDattr}" class="rsFlexBtnContainer rsBtnGrid${typeOfinp}" width="100%">`;
+	  
     let blnInBtnDiv = false;
     let intNumBtnInDiv = 0;
     let blnMakeNormalBtn = true;
